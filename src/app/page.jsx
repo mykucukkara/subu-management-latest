14f19406a3fde321171d294c78a7029a71e152e1
yusufkaraasln <yusufkaraasln@gmail.com>
2024-07-30 20:24:43 +0300

    updated domain name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import Logout from "../components/button/Logout";
 
 export const getUser = async () =>
-  fetch("https://subu-management.vercel.app/api/user", {
+  fetch("https://subu-management-yusufkaraaslns-projects.vercel.app/", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
